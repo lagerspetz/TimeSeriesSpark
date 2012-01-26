6e286cda4dab68d001c918e6c5b3dbbb37b108e5
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-01-25 20:06:58 -0800

    Allow preparing localhost.

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -5,6 +5,25 @@ source $dir/common.sh
 
 
 aptstuff () {
+  if [ "$1" == "localhost" ]
+  then
+  # Increase apt cache limit, install basic versions of deps
+  echo 'APT::Cache-Limit 50000000;' | sudo tee /etc/apt/apt.conf
+  sudo apt-get update
+  sudo aptitude -y install $( cat deps.txt )
+
+  # Enable Debian Unstable, install scala 2.9.1 or later
+  echo 'deb http://ftp.debian.org/debian unstable main contrib non-free
+deb-src http://ftp.debian.org/debian unstable main contrib non-free' | sudo tee /etc/apt/sources.list.d/debian-unstable-for-scala.list
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AED4B06F473041FA
+  sudo apt-get update
+  sudo apt-get -y install scala
+
+  # Reset apt sources to current distribution
+  echo '# deb http://ftp.debian.org/debian unstable main contrib non-free
+# deb-src http://ftp.debian.org/debian unstable main contrib non-free' | sudo tee /etc/apt/sources.list.d/debian-unstable-for-scala.list
+  sudo apt-get update
+  else
   # Increase apt cache limit, install basic versions of deps
   ssh -t "$1" "echo 'APT::Cache-Limit 50000000;' | sudo tee /etc/apt/apt.conf"
   ssh -t "$1" sudo apt-get update
@@ -21,6 +40,7 @@ deb-src http://ftp.debian.org/debian unstable main contrib non-free' | sudo tee 
   ssh -t "$1" "echo '# deb http://ftp.debian.org/debian unstable main contrib non-free
 # deb-src http://ftp.debian.org/debian unstable main contrib non-free' | sudo tee /etc/apt/sources.list.d/debian-unstable-for-scala.list"
   ssh -t "$1" sudo apt-get update
+  fi
 }
 
 hdfs () {
@@ -32,9 +52,16 @@ hdfs () {
 }
 
 selfssh () {
+  if [ "$1" == "localhost" ]
+  then
+  ssh-keygen -t dsa -P "''" -f ~/.ssh/id_"$1"
+  ln -s id_"$1" .ssh/id_dsa
+  cat ~/.ssh/id_$1.pub >> ~/.ssh/authorized_keys
+  else
   ssh "$1" ssh-keygen -t dsa -P "''" -f ~/.ssh/id_"$1"
   ssh "$1" ln -s id_"$1" .ssh/id_dsa
   ssh "$1" "cat ~/.ssh/id_$1.pub >> ~/.ssh/authorized_keys"
+  fi
 }
 
 for k in $slaves; do
