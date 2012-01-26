35b50441192f3490ebec6d8d63164aafb58c48b5
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-01-26 01:43:35 -0800

    Fixed Spark handling of rates: make CaratRate serializable.

--- src/edu/berkeley/cs/amplab/carat/CaratRate.scala
+++ src/edu/berkeley/cs/amplab/carat/CaratRate.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable.HashSet
 class CaratRate(var uuid:String, val os:String, val model:String,
     val timeDiff:Long, val batteryDiff:Double,
     val events1:Seq[String], val events2:Seq[String],
-    val apps1:Seq[String], val apps2:Seq[String]) extends Ordered[CaratRate]{
+    val apps1:Seq[String], val apps2:Seq[String]) extends Ordered[CaratRate] with Serializable{
   
   def this(uuid: String, os:String, model:String, time1: Long, time2:Long, battery1:Double, battery2:Double, events1:String, events2: String, apps1:String, apps2:String){
     this(uuid, os, model, time2-time1, battery2-battery1, events1.split(" "), events2.split(" "), apps1.split(" "), apps2.split(" "))
