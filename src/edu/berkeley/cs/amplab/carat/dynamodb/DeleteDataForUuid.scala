a0f0ca9e616f2becde2366f2916d31dc9851ec01
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-02-02 12:12:22 -0800

    Added a data deletion program. Just lists the data for now.

---
+++ src/edu/berkeley/cs/amplab/carat/dynamodb/DeleteDataForUuid.scala
@@ -0,0 +1,23 @@
+package edu.berkeley.cs.amplab.carat.dynamodb
+
+import edu.berkeley.cs.amplab.carat._
+import com.amazonaws.services.dynamodb.model.AttributeValue
+import collection.JavaConversions._
+
+/**
+ * Program to copy analyzed data of a certain uuid under osVersion 5.0 for Simulator data access.
+ */
+object DeleteDataForUuid {
+  def main(args: Array[String]) {
+    if (args != null && args.length == 1) {
+      deleteDataForUuid(args(0))
+    }
+  }
+
+  def deleteDataForUuid(uuId: String) {
+    if (uuId != null) {
+      println("Getting stuff from "+samplesTable)
+      var res = DynamoDbDecoder.getItems(samplesTable, uuId)
+    }
+  }
+}
