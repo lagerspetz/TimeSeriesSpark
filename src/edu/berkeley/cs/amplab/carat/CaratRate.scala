2afcc5f8af144871f8d03c7f3e0ef16ecb3c85dc
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-02-09 14:23:22 -0800

    Check if the new last zero samples broke the analysis.

--- src/edu/berkeley/cs/amplab/carat/CaratRate.scala
+++ src/edu/berkeley/cs/amplab/carat/CaratRate.scala
@@ -24,7 +24,10 @@ class CaratRate(var uuid:String, val os:String, val model:String,
   def rate() = {
     // batteryDiff is between 0 and 1, negative. Multiply by -100.0 to get 0 to 100, positive.
     // The unit for rate is percent per second.
-    batteryDiff * -100.0 / timeDiff
+    if (timeDiff == 0)
+      0
+    else
+      batteryDiff * -100.0 / timeDiff
   }
   
   def getAllApps() = {
