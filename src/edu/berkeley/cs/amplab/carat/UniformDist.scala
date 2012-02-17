b77a7f3fdf026c637cbb35e4dba78b201a8d284a
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-02-17 12:39:42 -0800

    Improved debug msg.

--- src/edu/berkeley/cs/amplab/carat/UniformDist.scala
+++ src/edu/berkeley/cs/amplab/carat/UniformDist.scala
@@ -46,7 +46,7 @@ class UniformDist(val from:Double, val to:Double) extends Ordered[UniformDist] w
       val upperBound = { if (end < to) end else to }
       assert(upperBound <= to, "upperBound should be within the range")
       val p = (upperBound - lowerBound) * prob
-      assert(p <= prob, "probOverlap should not be greater than prob")
+      assert(p <= prob, "probOverlap should not be greater than prob:" + p + ", " + prob)
       p
     }
   }
