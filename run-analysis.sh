925d21829d4bce63cbd2a2d481f753a99d587409
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-02-25 13:53:07 -0800

    Added analysis script.

---
+++ run-analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Main script for running the Carat data analysis.
+while true
+do
+  d=$( date +%Y-%m-%d-%H-%M-%S ).txt
+  echo "Analysis started at $d"
+  ./run-laptop.sh edu.berkeley.cs.amplab.carat.CaratDynamoDataAnalysis DEBUG \
+  &> "carat-dynamo-data-analysis-log-$d.txt"
+  d=$( date +%Y-%m-%d-%H-%M-%S ).txt
+  echo "Analysis finished at $d"
+  rm -rf spark-temp/spark-local
+  sleep 3600
+done
