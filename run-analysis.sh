7d97b73b7142afb8075e16200f6304302b28894d
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-02-27 21:59:43 -0800

    do not repeat .txt

--- run-analysis.sh
+++ run-analysis.sh
@@ -2,11 +2,11 @@
 # Main script for running the Carat data analysis.
 while true
 do
-  d=$( date +%Y-%m-%d-%H-%M-%S ).txt
+  d=$( date +%Y-%m-%d-%H-%M-%S )
   echo "Analysis started at $d"
   ./run-laptop.sh edu.berkeley.cs.amplab.carat.CaratDynamoDataAnalysis DEBUG \
   &> "carat-dynamo-data-analysis-log-$d.txt"
-  d=$( date +%Y-%m-%d-%H-%M-%S ).txt
+  d=$( date +%Y-%m-%d-%H-%M-%S )
   echo "Analysis finished at $d"
   rm -rf spark-temp/spark-local-*
   sleep 3600
