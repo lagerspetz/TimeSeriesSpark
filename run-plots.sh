bc3528e0a8af866ec43339f423c217a1a4edcd3b
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-03-02 22:43:31 -0800

    added log dir to plots script.

--- run-plots.sh
+++ run-plots.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Script for Carat data analysis to plots instead of the database.
+if [ ! -d logs ]; then mkdir logs; fi
 
 d=$( date +%Y-%m-%d-%H-%M-%S )
 echo "Plotting started at $d"
 ./run-laptop.sh edu.berkeley.cs.amplab.carat.PlotAndMakeAvailable \
-&> "carat-plots-log-$d.txt"
+&> "logs/carat-plots-log-$d.txt"
 d=$( date +%Y-%m-%d-%H-%M-%S )
 echo "Plotting finished at $d"
 echo "Removing temporary files"
