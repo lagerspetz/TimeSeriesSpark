f6e743c64270d399eaa938ea5a4419801684b976
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-03-15 17:26:12 -0700

    Add class.

--- run-laptop.sh
+++ run-laptop.sh
@@ -7,5 +7,6 @@ SPARK_MEM="2g"
 export SPARK_JAVA_OPTS="$JAVA_OPTS"
 export MASTER='local[1]'
 
-scala -cp "$SPARK_CLASSPATH" $class $MASTER $*
+
+scala -cp "$SPARK_CLASSPATH" $1 $MASTER $2 $3 $4 $5 $6
 
